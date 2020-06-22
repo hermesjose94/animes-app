445eceeb708552c96325d3139d149930ee4db2ab
Hermes Perez <hermesjose94@gmail.com>
2020-06-21 23:58:55 -0400

    error images

--- src/API/config.js
+++ src/API/config.js
@@ -1,3 +1,4 @@
-const url = 'https://hermesjose94-anime-api.herokuapp.com/api';
+// const url = 'https://hermesjose94-anime-api.herokuapp.com/api';
+const url = 'http://localhost:3002/api';
 const prueba = true;
 export default { url, prueba };
