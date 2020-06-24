84ce33e6aebb5d31e69e0d644409ddf9aed66766
Hermes Perez <hermesjose94@gmail.com>
2020-06-24 19:15:32 -0400

    arrglos finales

---
+++ build/precache-manifest.402abeb852d3a46fcfb3e470ef478ca1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd08e43a8ef5e03e6696f83f36ff330a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f489c6aef946ee117bb0",
+    "url": "/static/css/main.dad16085.chunk.css"
+  },
+  {
+    "revision": "9e8c71975b87fc65d5af",
+    "url": "/static/js/2.4dbea071.chunk.js"
+  },
+  {
+    "revision": "4d6ab1395afd48c38ab429e9c82a1cee",
+    "url": "/static/js/2.4dbea071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f489c6aef946ee117bb0",
+    "url": "/static/js/main.239747de.chunk.js"
+  },
+  {
+    "revision": "b0e0c0fdc2b9407457f3",
+    "url": "/static/js/runtime-main.9746b646.js"
+  },
+  {
+    "revision": "fb0c6b5030816b232f5a144bc2095f4e",
+    "url": "/static/media/astronaut.fb0c6b50.png"
+  },
+  {
+    "revision": "de07e55337d8bfa6ee6401f365a33ea1",
+    "url": "/static/media/background.de07e553.jpg"
+  },
+  {
+    "revision": "9c15144eb1980c4f43f1f433f698f037",
+    "url": "/static/media/logo.9c15144e.png"
+  },
+  {
+    "revision": "46424e894b6caf5357bff5480233cbb7",
+    "url": "/static/media/logoBanner.46424e89.png"
+  },
+  {
+    "revision": "9db9d7422140d94242fad98a3878b1f6",
+    "url": "/static/media/p404.9db9d742.png"
+  },
+  {
+    "revision": "bfc14033c83348541f4d06c6f6cd23a2",
+    "url": "/static/media/planet1.bfc14033.png"
+  },
+  {
+    "revision": "ca7c26937ebf18e53a298115ae4950af",
+    "url": "/static/media/planet2.ca7c2693.png"
+  },
+  {
+    "revision": "ba6946fd5609722c10422f1d2c6f7dc3",
+    "url": "/static/media/rocket.ba6946fd.png"
+  }
+]);
